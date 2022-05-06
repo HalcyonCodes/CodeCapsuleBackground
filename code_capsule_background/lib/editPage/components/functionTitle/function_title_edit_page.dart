ef2a6a381ed797bea555e645c57b3daf0e8fd0f5
qq130040200 <130040200@qq.com>
2022-05-06 11:34:43 +0800

    2022/5/6

--- code_capsule_background/lib/editPage/components/functionTitle/function_title_edit_page.dart
+++ code_capsule_background/lib/editPage/components/functionTitle/function_title_edit_page.dart
@@ -13,6 +13,7 @@ class FunctionTitle extends StatefulWidget {
 }
 
 class _FunctionTitleState extends State<FunctionTitle> {
+  GlobalKey KeyContainer = GlobalKey();
   GlobalKey keyFirst = GlobalKey();
   GlobalKey keySecond = GlobalKey();
   GlobalKey keyThired = GlobalKey();
@@ -29,13 +30,17 @@ class _FunctionTitleState extends State<FunctionTitle> {
       widget.pageUtil.setIsPageViewBuild(true);
       print(widget.pageUtil.isPageViewBuild!);
       RenderBox renderBox =
-          keyFirst.currentContext!.findRenderObject()! as RenderBox;
-      initY = renderBox.localToGlobal(Offset.zero).dy;
-      firstX = renderBox.localToGlobal(Offset.zero).dx;
+          KeyContainer.currentContext!.findRenderObject()! as RenderBox;
+      containerX = renderBox.localToGlobal(Offset.zero).dx;
+      containerY = renderBox.localToGlobal(Offset.zero).dy;
+      renderBox = keyFirst.currentContext!.findRenderObject()! as RenderBox;
+      initY = renderBox.localToGlobal(Offset.zero).dy - containerY;
+      firstX = renderBox.localToGlobal(Offset.zero).dx - containerX;
       renderBox = keySecond.currentContext!.findRenderObject()! as RenderBox;
-      secondX = renderBox.localToGlobal(Offset.zero).dx;
+      secondX = renderBox.localToGlobal(Offset.zero).dx - containerX;
       renderBox = keyThired.currentContext!.findRenderObject()! as RenderBox;
-      thiredX = renderBox.localToGlobal(Offset.zero).dx;
+      thiredX = renderBox.localToGlobal(Offset.zero).dx - containerX;
+
       refreshUi();
     });
   }
@@ -44,6 +49,8 @@ class _FunctionTitleState extends State<FunctionTitle> {
   double firstX = 0;
   double secondX = 0;
   double thiredX = 0;
+  double containerX = 0;
+  double containerY = 0;
 
   void refreshUi() {
     setState(() {});
@@ -52,6 +59,7 @@ class _FunctionTitleState extends State<FunctionTitle> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      key: KeyContainer,
       width: 1920 - 112 - 24 - 24 - 24,
       height: 55,
       padding: const EdgeInsets.only(left: 22, right: 22, top: 12, bottom: 6),
@@ -67,81 +75,102 @@ class _FunctionTitleState extends State<FunctionTitle> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      KString.KEditEditTitle,
-                      style: KFont.functionStyle,
-                    ),
-                    const SizedBox(
-                      height: 6,
-                    ),
-                    Container(
-                      key: keyFirst,
-                      width: 40,
-                      height: 6,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(3),
+                child: InkWell(
+                  onTap: () {
+                    widget.pageUtil.pageController!.animateToPage(0,
+                        duration: const Duration(milliseconds: 200),
+                        curve: Curves.linear);
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        KString.KEditEditTitle,
+                        style: KFont.functionStyle,
                       ),
-                    )
-                  ],
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      Container(
+                        key: keyFirst,
+                        width: 40,
+                        height: 6,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
               const SizedBox(
                 width: 20,
               ),
               SizedBox(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      KString.KEditCategoryTitle,
-                      style: KFont.functionStyle,
-                    ),
-                    const SizedBox(
-                      height: 6,
-                    ),
-                    Container(
-                      key: keySecond,
-                      width: 40,
-                      height: 6,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(3),
+                child: InkWell(
+                  onTap: () {
+                    widget.pageUtil.pageController!.animateToPage(1,
+                        duration: const Duration(milliseconds: 200),
+                        curve: Curves.linear);
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        KString.KEditCategoryTitle,
+                        style: KFont.functionStyle,
                       ),
-                    )
-                  ],
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      Container(
+                        key: keySecond,
+                        width: 40,
+                        height: 6,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
               const SizedBox(
                 width: 20,
               ),
               SizedBox(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      KString.KEditTagTitle,
-                      style: KFont.functionStyle,
-                    ),
-                    const SizedBox(
-                      height: 6,
-                    ),
-                    Container(
-                      key: keyThired,
-                      width: 40,
-                      height: 6,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(3),
+                child: InkWell(
+                  onTap: () {
+                    widget.pageUtil.pageController!.animateToPage(2,
+                        duration: const Duration(milliseconds: 200),
+                        curve: Curves.linear);
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        KString.KEditTagTitle,
+                        style: KFont.functionStyle,
                       ),
-                    )
-                  ],
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      Container(
+                        key: keyThired,
+                        width: 40,
+                        height: 6,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               )
             ],
